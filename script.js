d0e35734ebed9646b297c1929fde2cf77a1b8afc
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-11 22:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function validEmail(str) {
-  //your JS code here.
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(str);
 }
+
+// Test cases
+console.log(validEmail('abc@example.com')); // returns true
+console.log(validEmail('xyz@abc.com.in')); // returns true
+console.log(validEmail('john.doe@com.')); // returns false
+console.log(validEmail('@example.com')); // returns false
+console.log(validEmail('')); // returns false
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
